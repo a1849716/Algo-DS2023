f90a3a2f2e9716f76b3bae27ccb6fa04fdd9fea5
Richie Xiao <a1849716@adelaide.edu.au>
2023-03-18 22:39:23 +1030

    save

--- Reverser.cpp
+++ Reverser.cpp
@@ -12,9 +12,6 @@ int Reverser::reverseDigit(int value) {
   if (value <= 1) {
     return reversedValue;
   }
-  else if (value >= 0){
-    return -1;
-  }
   remainder = value % 10;
   reversedValue = reversedValue * 10 + remainder;
   return reverseDigit(value / 10);
