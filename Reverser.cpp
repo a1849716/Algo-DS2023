993901c7422bb1f031fd64521f72ed13b963b1ea
Yuqi Xiao <a1849716@adelaide.edu.au>
2023-03-18 20:56:31 +1030

    save

--- Reverser.cpp
+++ Reverser.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 
 int Reverser::reverseDigit(int value) {
-  //int valueLength = to_string(value).length();
-  int remainder = value%10;
-  return reverseDigit(remainder * 10 + (value%10));
+  // int valueLength = to_string(value).length();
+  if (value > 10) {
+    return value;
+  }
+  int remainder = value % 10;
+  return reverseDigit(remainder * 10 + (value % 10));
 };
 
 string Reverser::reverseString(string character){
-
+  
 };
