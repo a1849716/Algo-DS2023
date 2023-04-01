961dd987a88a5a65074f9f10dcf1ba248e736f75
Richie Xiao <a1849716@adelaide.edu.au>
2023-04-01 20:21:32 +1030

    save

--- Finder.cpp
+++ Finder.cpp
@@ -5,17 +5,17 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        int index = 0;
         for(size_t i = 1; i <= s2.size(); i++) {
             size_t found = s1.find(s2.substr(0, i));
+            int index = found+i-1;
             if (found != string::npos) {
                 result.push_back(found);
-                index++;
             } else {
                 break;
             }
         }
-        for (size_t j=index;j<=s2.size();j++){
+        
+        for (size_t j = 1; j<s2.size();j++){
             result.push_back(-1);
         }
         return result;
