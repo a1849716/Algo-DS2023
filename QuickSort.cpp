062ea9200c9ff4c558034c6b51323aab94354431
Yuqi Xiao <a1849716@adelaide.edu.au>
2023-04-11 15:39:43 +0930

    save

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,41 +2,43 @@
 
 using namespace std;
 
-void QuickSort::quickSort(vector<int> &list, int start, int end) {
-  int pivot_index = 0;
-  int pivot_value;
-  int length = list.size() - 1;
-  if (list.size() < 3) {
-    pivot_value = list.at(list.size() - 1);
+vector<int> QuickSort::sort(vector<int> list) {
+  int length = list.size();
+  int pivot;
+  if (length < 3) {
+    pivot = list.at(length - 1);
   } else {
-    pivot_value = list.at(2);
+    pivot = list.at(2);
   }
-
-  swap(pivot_value, list.at(length));
-
-  int i = 0;
-  int j = length;
-  while (i < j){
-    for (int j = length; j <= i+1; j--) {
-      if (list.at(i) > pivot_value && list.at(j) <= pivot_value) {
-        swap(list.at(i), list.at(j));
-      }
-      if (j > i) {
-        pivot_index = j;
-      }
-    }
-    i++;
-  }
-  swap(list.at(pivot_index), list.at(length));
 
   vector<int> lessThan;
-  vector<int> moreThan;
+  vector<int> greaterThan;
+  vector<int> sameNum;
 
+  for (int i = 0; i < length; i++) {
+    int currNum = list.at(i);
+    if (currNum < pivot) {
+      lessThan.push_back(currNum);
+    } else if (currNum > pivot) {
+      greaterThan.push_back(currNum);
+    } else if (currNum == pivot) {
+      sameNum.push_back(currNum);
+    }
+  }
+
+  sort(lessThan);
+  sort(greaterThan);
+
+  list.clear();
+  list = lessThan;
+
+  for (int j = 0; j < sameNum.size(); j++){
+    list.push_back(pivot);
+  }
+
+  for (int k = 0; k < greaterThan.size(); k++){
+    list.push_back(greaterThan.at(k));
+  }
   
-
-}
-
-vector<int> QuickSort::sort(vector<int> list) {
-  quickSort(list, 0, list.size() - 1);
   return list;
 };
