72b16eb10ac5368e6c0d3fc1d3bfd708e3924c1b
Richie Xiao <a1849716@adelaide.edu.au>
2023-05-20 17:04:49 +0930

    save

--- klargest.cpp
+++ klargest.cpp
@@ -9,9 +9,16 @@ int kth_largest(vector<int> values, int k){
     for (int i = 0; i < values.size(); i++){
         list.push(values.at(i));
     }
-    for (int j = 0; j < k; j++){
+    for (int j = 0; j < k-1; j++){
         list.pop();
     }
     
     return list.top();
 }
+
+/*
+int main(){
+    vector<int> val = {1,2,4,5,6,3};    
+    cout <<"second largest: "<< kth_largest(val, 2)<<endl;
+}
+*/
